40eea1c0bbd3be6ceb1fb43d2b3f8467713e39b3
Sainkee8878 <168168695+Sainkee8878@users.noreply.github.com>
2024-04-26 13:50:04 +0530

    Update program2.js
    program 2

--- program2.js
+++ program2.js
@@ -1,4 +1,35 @@
 function longestSubstring(s) {
+          let longestLength = 0;
+        let startOfSubstring = 0;
+        const characterMap = {};
+    
+        for (let i = 0; i < s.length; i++) {
+            const currentCharacter = s[i];
+            if (characterMap[currentCharacter] >= startOfSubstring) {
+                startOfSubstring = characterMap[currentCharacter] + 1;
+            }
+            characterMap[currentCharacter] = i;
+            const currentSubstringLength = i - startOfSubstring + 1;
+            longestLength = Math.max(longestLength, currentSubstringLength);
+        }
+    
+        return longestLength;
+    }
+    
+    module.exports = { longestSubstring };
+    
+    
+
+    
+    
+    module.exports = { longestSubstring };
+    
+
+    // Implementation of longestSubstring function
+}
+
+module.exports = { longestSubstring };
+
 
 
     // Implementation of longestSubstring function
